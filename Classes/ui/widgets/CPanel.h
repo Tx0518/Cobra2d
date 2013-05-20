bfb38202357aaeef2c89408b83d741b705b80d10
nwpu053741 <zhaochaozcx@163.com>
2013-05-20 15:19:57 +0800

    ！ 优化PANEL

--- Classes/ui/widgets/CPanel.h
+++ Classes/ui/widgets/CPanel.h
@@ -1,11 +1,22 @@
 #ifndef __C_PANEL_H__
 #define __C_PANEL_H__ 
-#include "CBaseContainer.h"
+/************************************************************************/
+/* this file is used to implement a simple panel widget
+you can add child widget to it
+*/
+/************************************************************************/
+class CBaseContainer;
 class CPanel:public CBaseContainer
 {
 public:
+	DECLARE_DYNCREATE(CPanel);
+	//////////////////////////////////////////////////////////////////////////
 	CPanel(void);
 	virtual ~CPanel(void);
+	//////////////////////////////////////////////////////////////////////////
+	//property
+	static const std::string PANEL_TYPE;
+	static const std::string PANEL_BK_PIC;
 	//////////////////////////////////////////////////////////////////////////
 	void draw(CGraphic* pGraphic);
 };
